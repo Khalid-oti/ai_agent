3d0d07d04797624124f522bf438ec1c26e566702
Khalid-oti <otaibik2233@gmail.com>
2025-06-23 21:40:00 +0300

    D: added error exception for prompt input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,8 +7,11 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
-
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+try:
+    prompt = sys.argv[1]
+except Exception as e:
+    print(e)
+    sys.exit(1)
 generated_content = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
 response = generated_content.text
 prompt_tokens = generated_content.usage_metadata.prompt_token_count
