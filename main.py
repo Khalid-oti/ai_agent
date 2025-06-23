76386c9164caa621cc32e92a927cdd4b31c65726
Khalid-oti <otaibik2233@gmail.com>
2025-06-23 11:57:00 +0300

    initialized git and added generate content method

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+generated_content = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+response = generated_content.text
+print(response)
