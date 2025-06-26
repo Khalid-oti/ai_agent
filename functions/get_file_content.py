a1e31df02b171da16e11094f0c82e2406d8e6780
Khalid-oti <otaibik2233@gmail.com>
2025-06-26 15:59:42 +0300

    K: added get file content function

---
+++ functions/get_file_content.py
@@ -0,0 +1,20 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    char_limit = 10000
+    try:
+        working_dir_path = os.path.abspath(working_directory)
+        full_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+        if not full_file_path.startswith(working_dir_path):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(full_file_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        with open(full_file_path, "r") as r:
+            file_content = r.read()
+        file_chars = list(file_content)
+        if len(file_chars) > char_limit:
+            truncated_file_content = file_content[:10000]
+            return truncated_file_content + f'...File "{file_path}" truncated at 10000 characters'
+        return file_content
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,33 +1,10 @@
-from functions.get_files_info import get_files_info
-import unittest
-import os
+from functions.get_file_content import get_file_content
 
-def test_files_info_function():
-    print(get_files_info("calculator", None))
-    print(get_files_info("calculator", "."))
-    print(get_files_info("calculator", "pkg"))
-    print(get_files_info("calculator", "/bin"))
-    print(get_files_info("calculator", "../"))
+def test():
+    print(get_file_content("calculator", "main.py"))
+    print("---------------------------------------")
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print("---------------------------------------")
+    print(get_file_content("calculator", "/bin/cat"))
 
-test_files_info_function()
-
-
-#class FileInfoTest(unittest.TestCase):
-    #def test_calculator_info1(self):
-        #expected = f"""- pkg: filesize={os.path.getsize("calculator/pkg/")}, is_dir=True\n
-        #- main.py: filesize={os.path.getsize("calculator/main.py/")}, is_dir=False\n
-        #- tests.py: filesize={os.path.getsize("calculator/tests.py/")}, is_dir=True\n"""
-        #get_files_info("calculator", ".") 
-        #self.assertEqual(expected, actual)
-
-    #def test_calculator_info2(self):
-        #get_files_info("calculator", "pkg")
-        #self.assertEqual(expected, actual)
-
-    #def test_calculator_info3(self):
-        #get_files_info("calculator", "/bin")
-        #self.assertEqual(expected, actual)
-
-    #def test_calculator_info4(self):
-        #get_files_info("calculator", "../")
-        #self.assertEqual(expected, actual)
+test()
